6898d7a122faf26eb5ff35c80df9c14e28286676
DrakiorTV <fandedrakior@gmail.com>
2024-01-07 17:58:48 +0100

    Classe Compte finalisée Exemple du fonctionnement de la classe Compte.

--- TP 3 GitHub/Compte.cs
+++ TP 3 GitHub/Compte.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace TP_3_GitHub
 {
-    internal class Compte
+    public class Compte
     {
+
+        public int ID {  get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public decimal Montant { get; set; }
+        public int Code { get; set; }
+
+
+        public void ConsulterSolde()
+        {
+            Console.WriteLine("BANQUE : Solde: " + Montant + " euros");
+        }
+
+        public void AjouterArgent(decimal montant)
+        {
+            Montant += montant;
+            Console.WriteLine("BANQUE: + "+ montant + " euros ajoutés au compte de "  + Nom  + Prenom);
+        }
+
+        public void RetirerArgent(decimal montant)
+        {
+            if (Montant >= montant)
+            {
+                Montant -= montant;
+                Console.WriteLine("BANQUE : - " + montant + "euros sur le compte de " + Nom + Prenom);
+            }
+            else
+            {
+                Console.WriteLine("BANQUE : Vous ne possédez pas les fonds nécessaires afin d'effectuer un retrait.");
+            }
+        }
+
+        
+
     }
 }
--- TP 3 GitHub/Program.cs
+++ TP 3 GitHub/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_3_GitHub
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez entrer votre code:");
+
+            Compte Compte1 = new Compte
+            {
+                ID = 1,
+                Nom = "Palpatine",
+                Prenom = "Sidious",
+                Montant = 2003,
+                Code = int.Parse(Console.ReadLine())
+
+            };
+
+            if (Compte1.Code == 0066) {
+
+                Console.WriteLine("Tapez 1 pour consulter le solde, tapez 2 pour ajouter de l'argent, tapez 3 pour retirer de l'argent, tapez autre chose pour quitter");
+
+                int.TryParse(Console.ReadLine(), out int menu);
+
+                if (menu == 1)
+                {
+                    Compte1.ConsulterSolde();
+                } 
+                else if (menu == 2)
+                {
+                    Compte1.AjouterArgent(int.Parse(Console.ReadLine()));
+                }
+                else if (menu == 3)
+                {
+                    Compte1.RetirerArgent(int.Parse(Console.ReadLine()));
+                } else
+                {
+                    Console.WriteLine("Vous quittez le menu.");
+                }
+
+            } else
+
+            {
+                Console.WriteLine("Le code saisie n'est pas reconnue.");
+            }
+
+            
+        }
+
+    }
+}
